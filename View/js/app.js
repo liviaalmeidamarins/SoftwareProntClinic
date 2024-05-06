0872a55c5ce88edcf250d12f622425e1dc094ecd
liviaalmeidamarins <lialmeidamarins@gmail.com>
2024-05-05 21:51:11 -0300

    modal formulario adicionada

---
+++ View/js/app.js
@@ -0,0 +1,18 @@
+ // Abre a modal ao clicar no link "Formulário"
+ document.getElementById('openModalBtn').addEventListener('click', function(){
+    document.getElementById('myModal').style.display = 'block';
+});
+
+// Fecha a modal ao clicar no botão fechar
+document.getElementsByClassName('close')[0].addEventListener('click', function(){
+    document.getElementById('myModal').style.display = 'none';
+});
+
+// Verifica se o CPF foi digitado e mostra os campos de nome e telefone se necessário
+document.getElementById('cpf').addEventListener('change', function(){
+    if(this.value === '') {
+        document.getElementById('notRegistered').style.display = 'block';
+    } else {
+        document.getElementById('notRegistered').style.display = 'none';
+    }
+});
