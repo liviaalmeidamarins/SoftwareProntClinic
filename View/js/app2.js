5cfd69ea9f0b08c9d96688a1dada0ce13b953e72
Joao Vinicius <joaoviniciusoliveira57@gmail.com>
2024-05-17 18:36:43 -0300

    validação de senha na tela cadastro

---
+++ View/js/app2.js
@@ -0,0 +1,38 @@
+// AQUI FICA O JS DA PAGINA DO VISITANTETE
+document.getElementById('formCadastro').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    let nome = document.getElementById('nomeCadastro').value.trim();
+    let sobrenome = document.getElementById('sobrenome').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let senha = document.getElementById('senha').value.trim();
+    let confirmarSenha = document.getElementById('confirmarSenhaCadastro').value.trim();
+    let termosAceitos = document.getElementById('agree-form').checked;
+    let message = document.getElementById('message');
+
+    message.style.color = 'red';
+
+    // Verifica se todos os campos estão preenchidos
+    if (!nome || !sobrenome || !email || !senha || !confirmarSenha) {
+        message.textContent = 'Por favor, preencha todos os campos!';
+        return;
+    }
+
+    // Verifica se as senhas coincidem
+    if (senha !== confirmarSenha) {
+        message.textContent = 'As senhas não coincidem!';
+        return;
+    }
+
+    // Verifica se os termos de serviço foram aceitos
+    if (!termosAceitos) {
+        message.textContent = 'Você deve aceitar os termos de serviço!';
+        return;
+    }
+
+    // Se todas as verificações passarem
+    message.style.color = 'green';
+    message.textContent = 'Cadastro realizado com sucesso!';
+
+
+})
